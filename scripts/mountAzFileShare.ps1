a982d38c68e9cb7628b153bd0b2a55658bf67612
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-08-22 11:24:00 +0200

    fix missing colon

--- scripts/mountAzFileShare.ps1
+++ scripts/mountAzFileShare.ps1
@@ -14,5 +14,5 @@ param(
 
 $secpassword = ConvertTo-SecureString $storageAccountKey -AsPlainText -Force
 $creds = New-Object System.Management.Automation.PSCredential("Azure\$storageAccountName", $secpassword)
-New-SmbGlobalMapping -RemotePath "\\$storageAccountName.file.core.windows.net\share" -Credential $creds -LocalPath $driveLetter -Persistent $true -RequirePrivacy $true
+New-SmbGlobalMapping -RemotePath "\\$storageAccountName.file.core.windows.net\share" -Credential $creds -LocalPath "$($driveLetter):" -Persistent $true -RequirePrivacy $true
 Invoke-Expression "icacls.exe $($driveLetter):\ /grant 'Everyone:(OI)(CI)(F)'"
