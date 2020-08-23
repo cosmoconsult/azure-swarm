cfb870941021d873879730216fe2f30f22e4bc9c
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-08-23 23:10:43 +0200

    fix call

--- scripts/mgrInitSwarmAndSetupTasks.ps1
+++ scripts/mgrInitSwarmAndSetupTasks.ps1
@@ -125,7 +125,7 @@ else {
     while ($tries -le 10) { 
         try {
             Write-Host "try to join (try $tries): $($secretJson.value)"
-            $job = start-job { Invoke-Expression $secretJson.value } 
+            $job = start-job { Invoke-Expression $($secretJson.value) } 
             $counter = 0
             while (($job.State -like "Running") -and ($counter -lt 4)) {
                 Write-Host "check $counter"
