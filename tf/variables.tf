d699c5893dabfc1688dd41f9ffaca1dc16807683
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-08-26 07:45:53 +0200

    wrong defaults

--- tf/variables.tf
+++ tf/variables.tf
@@ -10,7 +10,7 @@ variable "location" {
 variable "workerVmssSettings" {
   description = "The Azure VM scale set settings for the workers"
   default = {
-    size    = "Standard_E8ds_v4"
+    size    = "Standard_D8s_v3"
     number  = 2
     sku     = "2019-datacenter-core-with-containers"
     version = "17763.1158.2004131759"
@@ -53,12 +53,12 @@ variable "images" {
 
 variable "eMail" {
   description = "eMail address to be used for Let's Encrypt"
-  default     = "tobias.fenster@cosmoconsult.com"
+  default     = "change@me.com"
 }
 
 variable "additionalPreScriptWorker" {
   description = "additional script to call when setting up workers before the main script starts"
-  default     = "https://raw.githubusercontent.com/cosmoconsult/azure-swarm-bc/master/additionalPreScriptWorker.ps1"
+  default     = ""
 }
 
 variable "additionalPostScriptWorker" {
@@ -73,7 +73,7 @@ variable "additionalPreScriptMgr" {
 
 variable "additionalPostScriptMgr" {
   description = "additional script to call when setting up managers after the main script starts"
-  default     = "https://raw.githubusercontent.com/cosmoconsult/azure-swarm-bc/master/additionalPostScriptMgr.ps1"
+  default     = ""
 }
 
 variable "additionalPreScriptJumpbox" {
@@ -93,5 +93,5 @@ variable "dockerdatapath" {
 
 variable "authHeaderValue" {
   description = "authorization header value to be set when downloading custom scripts, if necessary"
-  default     = "token a575dab901bbaef2a4895db46072d2ead1146726"
+  default     = ""
 }
