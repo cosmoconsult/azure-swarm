1b23fd80eb91eb4c07cab3953713cbbef2b4ed11
Michael Megel <michael.megel@cosmoconsult.com>
2020-09-04 17:27:16 +0200

    try/catch for Invoke-WebRequest with/without headers

--- scripts/workerConfig.ps1
+++ scripts/workerConfig.ps1
@@ -204,7 +204,8 @@ if (-not $restart) {
                 'Authorization' = $authToken
             }
         }
-        Invoke-WebRequest -UseBasicParsing -Headers $headers -Uri $additionalPostScript -OutFile 'c:\scripts\additionalPostScript.ps1'
+        try { Invoke-WebRequest -UseBasicParsing -Headers $headers -Uri $additionalPostScript -OutFile 'c:\scripts\additionalPostScript.ps1' }
+        catch { Invoke-WebRequest -UseBasicParsing -Uri $additionalPostScript -OutFile 'c:\scripts\additionalPostScript.ps1' }
         & 'c:\scripts\additionalPostScript.ps1' -branch "$branch" -authToken "$authToken"
     }
 }
